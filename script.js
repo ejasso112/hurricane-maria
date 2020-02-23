c110b988132ebff36fbe539e610733d6b1560720
Eduardo Jasso <eduardo.jasso.per@gmail.com>
2020-02-22 21:03:07 -0600

    Updated

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ $(document).ready(function () {
 
 function onScroll(event) {
     var scrollPos = $(document).scrollTop();
-    $('.nav a').each(function () {
+    $('.site-nav a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('.nav ul li a').removeClass("active");
+            $('.site-nav a').removeClass("active");
             currLink.addClass("active");
         } else {
             currLink.removeClass("active");
